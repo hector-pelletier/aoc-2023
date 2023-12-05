cdb347ef4354b52e4d047b8aecb7ef2f94358c64
hpelleti <hector.pelletier@protonmail.com>
2023-12-05 21:32:08 +0100

    cleaned day 5

--- src/day05.hs
+++ src/day05.hs
@@ -9,40 +9,34 @@ getMaps acc (l:ls) = case l of
     "" -> acc : (getMaps [] $ tail $ ls)
     s -> getMaps (ins : acc) ls where ins = map read $ words $ s
 
-step :: [[Int]] -> Int -> Int
-step [] n = n
-step (ins:inss) n = if n >= s && n < s + l then t + (n - s) else step inss n
-    where [t,s,l] = ins 
+inter :: (Int, Int) -> (Int, Int) -> (Int, Int)
+inter (x1, l1) (x2, l2) = (x, (min (x1 + l1) (x2 + l2)) - x) where x = max x1 x2
 
-rangeStep :: [[Int]] -> (Int, Int) -> [(Int, Int)]
-rangeStep [] (s, l) = [(s, l)]
-rangeStep (ins:inss) (s, l)
+step :: [[Int]] -> (Int, Int) -> [(Int, Int)]
+step [] (s, l) = [(s, l)]
+step (ins:inss) (s, l)
     | l <= 0 = []
-    | b >= s && b + r <= s + l =
-        [(t, r)] ++ (rem (s, (b - s))) ++ (rem ((b + r), (s + l - b - r))) 
-    | b >= s && b < s + l && b + r > s + l =
-        [(t, s + l - b)] ++ (rem (s, (b - s)))
-    | b < s && b + r >= s + 1 && b + r < s + l =
-        [(t + (s - b), (b + r - s))] ++ (rem (b + r, l - (b + r - s)))
-    | b < s && b + r >= s + l = [(t + (s - b), l)]
+    | ri > 0 = rem (s, xi - s) ++ rem (xi + ri, s + l - xi - ri) ++ [(t + xi - b, ri)] 
     | otherwise = rem (s, l)
     where [t,b,r] = ins
-          rem = rangeStep inss
+          rem = step inss
+          (xi, ri) = inter (s, l) (b, r)
 
-allSteps :: [Int] -> [[[Int]]] -> [Int]
-allSteps = foldl (\d inss -> map (step inss) $ d)
+solve :: [(Int, Int)] -> [[[Int]]] -> Int
+solve seed = minimum . (map fst) .  allSteps
+    where allSteps = foldl (\d inss -> concat $ map (step inss) $ d) seed
 
-allRangeSteps :: [(Int, Int)] -> [[[Int]]] -> [(Int, Int)]
-allRangeSteps = foldl (\d inss -> concat $ map (rangeStep inss) $ d)
+ranges :: [Int] -> [(Int, Int)]
+ranges [] = []
+ranges (x:y:t) = (x, y) : (ranges t)
 
-pairs :: [Int] -> [(Int, Int)]
-pairs [] = []
-pairs (x:y:t) = (x, y) : (pairs t)
+trivialRanges :: [Int] -> [(Int, Int)]
+trivialRanges = foldl (\s x -> (x, 1) : s) []
 
 main :: IO ()
 main = do
     alma <- lines <$> getContents
     let seeds = getSeeds $ alma
     let maps = getMaps [] $ (tail . tail . tail) $ alma
-    print $ minimum $ allSteps seeds $ maps
-    print $ minimum $ map fst $ allRangeSteps (pairs seeds) $ maps
+    print $ solve (trivialRanges seeds) $ maps
+    print $ solve (ranges seeds) $ maps

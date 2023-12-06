65ca24b354758619db7f4599549384a79bc53c35
hpelleti <hector.pelletier@protonmail.com>
2023-12-06 08:34:34 +0100

    Small fix

--- src/day06.hs
+++ src/day06.hs
@@ -1,7 +1,7 @@
 -- Advent of code day 06
 
 nwins :: Double -> Double -> Int
-nwins t d =  (min (floor t) (floor ((t + del) / 2))) - (max 0 (ceiling ((t - del) / 2))) + 1
+nwins t d =  (min (floor t) (ceiling ((t + del) / 2) - 1)) - (max 0 (floor ((t - del) / 2) + 1)) + 1
     where del = sqrt (t ^ 2 - 4 * d)
 
 main :: IO ()

13d47b0841617436df39f62e8aab3d048c4c83b5
hpelleti <hector.pelletier@protonmail.com>
2023-12-06 22:16:37 +0100

    Cleaned day 4

--- src/day04.hs
+++ src/day04.hs
@@ -3,16 +3,19 @@
 import Data.List (break)
 import qualified Data.Set as Set
 
-wins :: [String] -> Int
-wins l = Set.size $ Set.intersection (Set.fromList a) (Set.fromList $ tail b) 
+parseCard :: [String] -> Int
+parseCard l = Set.size $ Set.intersection (Set.fromList a) (Set.fromList $ tail b) 
     where (a, b) = break (== "|") l
 
-mcardwins :: [Int] -> Int -> Int
-mcardwins cards = (map (cardwins cards) [0..] !!)
-    where cardwins cards n = 1 + (foldr (\a b -> b + (mcardwins cards a)) 0 [(n+1)..(n+(cards !! n))])
+addcards :: Int -> Int -> [Int] -> [Int]
+addcards n gain l = (map (+ n) $ take gain $ l) ++ (drop gain l)
+
+scratch :: [Int] -> [Int] -> Int -> Int
+scratch [] [] tot = tot
+scratch (gain:gains) (n:ns) tot = n + scratch gains (addcards n gain ns) tot
 
 main :: IO ()
 main = do
-    cards <- map (wins . tail . tail . words)  <$> lines <$> getContents
-    print $ sum $ map (\s -> 2 ^ (s - 1)) $ filter (> 0) $ cards 
-    print $ sum $ map (mcardwins cards) $ [0..(length cards - 1)]
+    gains <- map (parseCard . tail . tail . words)  <$> lines <$> getContents
+    print $ sum $ map (\s -> 2 ^ (s - 1)) $ filter (> 0) $ gains
+    print $ scratch gains (replicate (length gains) 1) $ 0

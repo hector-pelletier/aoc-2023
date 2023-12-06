47ab110f3b180b694395ad1b17a092a7465670ae
hpelleti <hector.pelletier@protonmail.com>
2023-12-06 07:28:33 +0100

    day 6

---
+++ src/day06.hs
@@ -0,0 +1,12 @@
+-- Advent of code day 06
+
+nwins :: Double -> Double -> Int
+nwins t d =  (min (floor t) (floor ((t + del) / 2))) - (max 0 (ceiling ((t - del) / 2))) + 1
+    where del = sqrt (t ^ 2 - 4 * d)
+
+main :: IO ()
+main = do
+    times <- tail . words <$> getLine
+    dists <- tail . words <$> getLine
+    print $ product $ map (\(t, d) -> nwins (read t) (read d)) $ zip times dists
+    print $ nwins (read $ concat times) (read $ concat dists)

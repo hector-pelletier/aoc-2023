baf2f19ab18f73bbc60ef8301f4372355c1cb26c
hpelleti <hector.pelletier@protonmail.com>
2023-12-11 08:04:58 +0100

    Performance improvement in day 11

--- src/day11.hs
+++ src/day11.hs
@@ -1,28 +1,29 @@
 -- Advent of code day 11
 
-import Data.List (nub, tails)
+import Data.List (tails)
 
-parseImage :: String -> [(Int, Int)]
-parseImage s = aux 0 0 [] s
-    where aux i j l []       = l
-          aux i j l ('\n':t) = aux (i+1) 0     l         t
-          aux i j l ('#':t)  = aux i     (j+1) ((i,j):l) t 
-          aux i j l (h:t)    = aux i     (j+1) l         t
+parseImage :: String -> ([(Int, Int)], Int, Int)
+parseImage s = aux 0 0 0 [] s
+    where aux i j mj l []       = (l, i, mj)
+          aux i j mj l ('\n':t) = aux (i+1)     0 (j-1)         l t
+          aux i j mj l ('#':t)  = aux     i (j+1)    mj ((i,j):l) t 
+          aux i j mj l (h:t)    = aux     i (j+1)    mj         l t
           
 pairs :: [a] -> [(a,a)]
-pairs l = [(x, y) | (x:xt) <- tails l, y <- xt]   
+pairs l = [(x, y) | (x:xt) <- tails l, y <- xt]
 
-expDist :: [(Int, Int)] -> Int -> ((Int, Int), (Int, Int)) -> Int
-expDist gxs f ((x1,y1),(x2,y2)) = dx + dy 
-    where (dx, dy) = (dist (map fst gxs) f x1 x2, dist (map snd gxs) f y1 y2)
-          dist gxs f a b | a == b = 0
-                         | a > b  = dist gxs f b a
-                         | a <= b = d + (f - 1) * (d - occ - 1)
-                            where d = b - a
-                                  occ = length $ nub $ filter (\x -> (x > a) && (x < b)) $ gxs
+ds :: [Int] -> Int -> Int -> [Int]
+ds gxs m f = [if a `elem` gxs then 1 else f | a <- [0..m]]
+
+dist :: [Int] -> [Int] -> ((Int, Int), (Int, Int)) -> Int
+dist dx dy ((x1,y1),(x2,y2)) = partial dx x1 x2 + partial dy y1 y2
+    where partial ds a b | a > b  = partial ds b a
+                         | a <= b = sum $ take (b-a) $ drop a $ ds
 
 main :: IO ()
 main = do
-    galaxies <- parseImage <$> getContents
-    print $ sum $ map (expDist galaxies 2) $ pairs $ galaxies
-    print $ sum $ map (expDist galaxies 1000000) $ pairs $ galaxies
+    (gxs, i, j) <- parseImage <$> getContents
+    let (sdx, sdy) = (ds (map fst gxs) i 2, ds (map snd gxs) j 2)
+    print $ sum $ map (dist sdx sdy) $ pairs $ gxs
+    let (gdx, gdy) = (ds (map fst gxs) i 1000000, ds (map snd gxs) j 1000000)
+    print $ sum $ map (dist gdx gdy) $ pairs $ gxs
